1a4bd3a9f7a4e777892acab35e8f5e331c1fc9d8
Conor Bradshaw <cpbshaw@gmail.com>
2017-09-30 13:27:37 +0100

    no

---
+++ Number Base Conversion.py
@@ -0,0 +1,12 @@
+import os
+
+def main():
+	os.system ("title Decimal To Binary")
+	os.system ("cls")
+	os.system ("color b")
+	fromNum = int(input("Number: "))
+	fromBase = int(input("From Base: "))
+	toBase = int(input("To Base: "))
+	print (str(convert(fromNum,)))
+	input ("Press Enter")
+	main()
